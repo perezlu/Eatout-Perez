e5b79c5e6dccd05daea882e23f0e4899aa7614c8
Luciana Perez <luliperezla@gmail.com>
2023-01-17 19:59:14 -0300

    terminando de arreglar el form de reservas

--- js/main.js
+++ js/main.js
@@ -20,24 +20,22 @@ function Reservar(nombreRes, apellidoRes, numAsistentes, diaReserva){
 //
 botonRes.addEventListener('click',() =>{
     nombreRes = document.getElementById('form4Example2').value
-
     apellidoRes = document.getElementById('form4Example25').value
-
     numAsistentes = document.getElementById('numNames').value
     diaReserva = document.getElementById('form4Example4').value;
-    if (nombreRes || apellidoRes || diaReserva == null || nombreRes || apellidoRes || diaReserva == "") {
+
+    if (nombreRes == null || apellidoRes == null || diaReserva == null || nombreRes == "" || apellidoRes == "" || diaReserva == "") {
         let p = document.createElement("p");
         p.innerText= "CASILLAS REQUERIDAS"
         const camposReq = document.getElementById('camposReq')
         camposReq.appendChild(p).style.color = 'red'
     }else{
     Reservar(nombreRes, apellidoRes, numAsistentes, diaReserva);
-    let confirmación = document.createElement("div")
-    confirmación.innerText = <span>Muchas gracias por su reserva! En los próximos minutos podrá recibir un mail a su cuenta donde aparecerán todos los datos de la reserva
-    en caso de no recibirlo, escribanos a cualquiera de nuestras redes sociales para solucionar el problema</span>
-    confirmación.innerText = <h3>Bon appetit!</h3>
+    let confirmación = document.createElement("span")
     const divConfirmacion = document.getElementById('confirmacion')
-    divConfirmación.appendChild(confirmacion)
+    confirmacion.innerText = "Muchas gracias por su reserva! En los próximos minutos podrá recibir un mail a su cuenta donde aparecerán todos los datos de la reserva en caso de no recibirlo, escribanos a cualquiera de nuestras redes sociales para solucionar el problema. Bon appetit!"
+
+    confirmacion.appendChild(divConfirmacion)
 }
 })
 
